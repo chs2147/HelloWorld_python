e3dad79ca2fea6a246c217b4632cde2fdf640fbf
HUI SHENG CHEN <chs2147@naver.com>
2017-01-03 15:24:42 +0900

    Class example - special methods added. __str__()

--- oop/oop_method.py
+++ oop/oop_method.py
@@ -1,6 +1,17 @@
 class Person:
+
+    message = ''
+
+    def __init__(self, message):
+        self.message = message
+
+    def __str__(self):
+        return ('My name is {0}').format(self.message)
+
     def say_hi(self):
         print('Hello, how are you?')
 
-p = Person()
+p = Person('Leslie')
 p.say_hi()
+
+print p
